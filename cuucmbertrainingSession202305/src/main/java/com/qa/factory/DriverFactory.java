167511e823d9f57895db75397d25a0c28b76893d
anokarao <anokarao@gmail.com>
2023-05-23 08:45:22 +0530

    new cucumber series--added code for driver factory

---
+++ cuucmbertrainingSession202305/src/main/java/com/qa/factory/DriverFactory.java
@@ -0,0 +1,45 @@
+package com.qa.factory;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class DriverFactory {
+	
+	public WebDriver driver;
+	
+	public static ThreadLocal<WebDriver> tlDriver=new ThreadLocal<>();
+	
+	/**
+	 * This method is used to initialize the thradlocal driver on the basis of given
+	 * browser
+	 * 
+	 * @param browser
+	 * @return this will return tldriver.
+	 */
+	public WebDriver initDriver(String browser) {
+		System.out.println("Browser name is :"+browser);
+		
+		if(browser.equalsIgnoreCase("chrome")) {
+			tlDriver.set(new ChromeDriver());		
+			}
+		else if(browser.equalsIgnoreCase("firefox")) {
+			tlDriver.set(new FirefoxDriver());
+		}
+		else if(browser.equalsIgnoreCase("safari")) {
+			tlDriver.set(new SafariDriver());
+		}
+		getDriver().manage().deleteAllCookies();
+		getDriver().manage().window().maximize();
+		getDriver().manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+		return getDriver();
+	}
+	
+	public static synchronized  WebDriver getDriver() {
+		return tlDriver.get();
+	}
+
+}
