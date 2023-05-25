82f5711c87409299b9c17b0274cebcf7d103d934
anokarao <anokarao@gmail.com>
2023-05-25 23:27:29 +0530

    Added files Config Reader, Hooks, Config properties

---
+++ cuucmbertrainingSession202305/src/main/java/com/qa/util/configReader.java
@@ -0,0 +1,32 @@
+package com.qa.util;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
+public class configReader {
+	private Properties prop;
+	
+	/**
+	 * This method is used to load the properties from config.properties file
+	 * @return it returns Properties prop object
+	 */
+	public Properties initProperties() {
+		prop=new Properties();
+		try {
+			FileInputStream fis=new FileInputStream("./src/test/resources/config/config.properties");
+			prop.load(fis);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return prop;
+	}
+
+}
--- cuucmbertrainingSession202305/src/test/java/AppHooks/Hooks.java
+++ cuucmbertrainingSession202305/src/test/java/AppHooks/Hooks.java
@@ -1,5 +1,47 @@
 package AppHooks;
 
+import java.util.Properties;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import com.qa.factory.DriverFactory;
+import com.qa.util.configReader;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+
 public class Hooks {
+	
+	private configReader configreader;
+	private DriverFactory driverFactory;
+	private WebDriver driver;
+	Properties prop;
+	
+	@Before(order=0)
+	public void getProperty() {
+		configreader=new configReader();
+		prop=configreader.initProperties();
+	}
+	
+	@Before(order=1)
+	public void launchBrowser() {
+		driverFactory=new DriverFactory();
+		driver=driverFactory.initDriver(prop.getProperty("browser"));
+	}
+	
+	@After(order=0)
+	public void quitDriver() {
+		driver.quit();
+	}
+	
+	public void tearDown(Scenario scenario) {
+		if(scenario.isFailed()) {
+			String filename=scenario.getName().replaceAll(" ", "_");
+			byte[] screenshot=((TakesScreenshot)driver).getScreenshotAs(OutputType.BYTES);
+			scenario.attach(screenshot, "image/png", filename);
+		}
+	}
 
 }
