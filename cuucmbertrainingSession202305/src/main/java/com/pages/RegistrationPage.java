25d19ed12afc28a5a0e6e25f05ef5ab1186a62ae
anokarao <anokarao@gmail.com>
2023-05-26 10:12:39 +0530

    registratin page changes

--- cuucmbertrainingSession202305/src/main/java/com/pages/RegistrationPage.java
+++ cuucmbertrainingSession202305/src/main/java/com/pages/RegistrationPage.java
@@ -1,6 +1,5 @@
 package com.pages;
 
-<<<<<<< HEAD
 import org.openqa.selenium.WebDriver;
 
 public class RegistrationPage {
@@ -11,8 +10,4 @@ public class RegistrationPage {
 	{
 		this.driver=driver;
 	}
-=======
-public class RegistrationPage {
-
->>>>>>> f09ed57 (folder structure added)
 }
